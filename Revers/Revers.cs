415ff02285cc16af1b1736b60dac84752b690831
Dudiak-Nikolas <Dudyak.kolya@gmail.com>
2020-09-09 03:21:04 +0300

    Init Commit 1

---
+++ Revers/Revers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Revers
+{
+    class Solution
+    {
+
+        public static int Reverse(int x)
+        {
+            int rev = 0;
+            while (x > 0)
+            {
+                rev = rev * 10 + x % 10;
+                x = x / 10;
+            }
+            return rev;
+        }
+
+        static void Main(String[] args)
+        { 
+              Console.Write(" Укажите Числа : ");              
+                Console.Write("Числа в реверсе : "
+                        + Reverse(Convert.ToInt32(Console.ReadLine())));
+        }
+    }
+}
